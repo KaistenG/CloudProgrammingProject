50883b62d95c280df42daf757c38978a8f52dd83
KaistenG <129497085+KaistenG@users.noreply.github.com>
2026-03-11 11:53:35 +0100

    Terraform Modulstruktur hinzugefügt

---
+++ terraform/modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Name der Ressourcengruppe"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
--- terraform/modules/static_website/variables.tf
+++ terraform/modules/static_website/variables.tf
@@ -1,23 +1,19 @@
-variable "rg_name" {
+variable "resource_group_name" {
   description = "Name der Ressourcengruppe"
   type        = string
-  default     = "cpp-resources"
 }
 
 variable "location" {
   description = "Azure Region"
   type        = string
-  default     = "germanywestcentral"
 }
 
 variable "sa_name" {
   description = "Name des Storage Accounts"
   type        = string
-  default     = "cppstorageaccount250321"
 }
 
 variable "tfstate_container_name" {
-  description = "Container für Terraform State"
+  description = "Name des Containers für Terraform State"
   type        = string
-  default     = "tfstate"
 }
